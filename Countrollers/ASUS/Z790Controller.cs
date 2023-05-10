e24d3577f92c337bbccd158a6c48bebaaf863b9f
UltraTank\HYTE <jimny1016@gmail.com>
2023-05-10 14:10:03 +0800

    Add asus z790-a controller.

---
+++ Countrollers/ASUS/Z790Controller.cs
@@ -0,0 +1,36 @@
+﻿using AuraServiceLib;
+using System;
+
+namespace LEDPlayground.Countrollers.ASUS
+{
+    public class Z790Controller
+    {
+        public Z790Controller()
+        {
+            // Create SDK instance
+            IAuraSdk sdk = new AuraSdk();// Aquire control
+            sdk.SwitchMode();
+
+            // enumerate all devices
+            IAuraSyncDeviceCollection devices = sdk.Enumerate(0);
+            foreach (IAuraSyncDevice dev in devices)
+            {
+                Console.WriteLine($"I'm device:{dev.Name}");
+                Console.WriteLine($"I'm device:{dev.Type}");
+                //if (dev.Lights.Count > 0) { Console.WriteLine($"I'm device:{dev.Name}"); Console.WriteLine($"I'm device:{dev.Type}"); }
+
+                //if (dev.Name != "AddressableStrip 3")
+                //    continue;
+                // Traverse all LED's
+                //foreach (IAuraRgbLight light in dev.Lights)
+                //{
+                //    //if(light.Name != "RGB HEADER")
+                //    //    continue;
+                //    Console.WriteLine($"I'm light:{light.Name}");
+                //    light.Color = 0x00FF0000;//0x00BBGGRR
+                //}
+                //dev.Apply();
+            }
+        }
+    }
+}
