7e911bced1f3d929788338302d047d5ca558d1ad
UltraTank\HYTE <jimny1016@gmail.com>
2023-05-15 17:02:19 +0800

    Add Corsair ver 0.2.

--- Countrollers/Corsair/LightingCommanderCoreController.cs
+++ Countrollers/Corsair/LightingCommanderCoreController.cs
@@ -1,18 +1,23 @@
 ﻿using HidSharp;
+using HueApi.Models;
 using LEDPlayground.Enums.Corsair;
+using Org.BouncyCastle.Bcpg;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LEDPlayground.Countrollers.Corsair
 {
     static class LightingCommanderCoreConfig
     {
         public const byte ConnectionType = 8;
-        public const int byteCount = 1280;
+        public const int ByteCount = 1280;
+        public const int FanTypesQL = 6;
     }
 
     public class LightingCommanderCoreController
@@ -33,8 +38,10 @@ namespace LEDPlayground.Countrollers.Corsair
                 handler.CloseAllHandle();
                 SetProperty();
                 handler.OpenHandle(Handles.Lighting, Endpoints.LightingController);
-                //SendRGBData(new byte[] { 255,255,255 });
-
+                SetFanType();
+                handler.OpenHandle(Handles.Background, Endpoints.LedCount_4Pin);
+                handler.CloseHandle(Handles.Background);
+                SendRGBData(GetPumpLedData("0xFFFFFF"));
             }
             catch (Exception ex)
             {
@@ -58,19 +65,73 @@ namespace LEDPlayground.Countrollers.Corsair
             };
             _stream.Write(packet.ToArray());
         }
+        public void SetFanType()
+        {
+            // Configure Fan Ports to use QL Fan size grouping. 34 Leds
+            List<byte> fanSettings = Enumerable.Repeat((byte)0, 25).ToList();
+            fanSettings[0] = 0x00;
+            fanSettings[1] = 0x08;
+            fanSettings[2] = 0x06;
+            fanSettings[3] = 0x01;
+            fanSettings[4] = 0x11;
+            fanSettings[8] = 0x0D;
+            fanSettings[10] = 0x07;
+            int offset = 11;
+
+            for (int iIdx = 0; iIdx < 7; iIdx++)
+            {
+                fanSettings[offset + iIdx * 2] = 0x01;
+                fanSettings[offset + iIdx * 2 + 1] = (byte)(iIdx == 0 ? 0x01 : LightingCommanderCoreConfig.FanTypesQL); // 1 for nothing, 0x08 for pump?
+            }
+            _stream.Write(fanSettings.ToArray());
+
+        }
+        public static byte[] GetPumpLedData(string color)
+        {
+            List<byte> RGBData = new List<byte>();
+
+            for (int iIdx = 0; iIdx < EliteLCDCooler.Mapping.Count; iIdx++)
+            {
+                List<byte> mxPxColor;
+
+                //find colors
+                mxPxColor = HexToRgb(color);
+
+                //set colors
+                RGBData.Add(mxPxColor[0]);
+                RGBData.Add(mxPxColor[1]);
+                RGBData.Add(mxPxColor[2]);
+                //RGBData[EliteLCDCooler.Mapping[iIdx] * 3] = mxPxColor[0];
+                //RGBData[EliteLCDCooler.Mapping[iIdx] * 3 + 1] = mxPxColor[1];
+                //RGBData[EliteLCDCooler.Mapping[iIdx] * 3 + 2] = mxPxColor[2];
+            }
+
+            return RGBData.ToArray();
+        }
+
+        public static List<byte> HexToRgb(string hex)
+        {
+            hex = hex.Replace("0x", "");
+            var rgb = new List<byte>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                rgb.Add(byte.Parse(hex.Substring(i * 2, 2), System.Globalization.NumberStyles.HexNumber));
+            }
+
+            return rgb;
+        }
+
         private static void SendRGBData(byte[] rgbData)
         {
             const int initialHeaderSize = 8;
             const int headerSize = 4;
             int bytesSent = 0;
 
-            if (false)
-            {
-                rgbData = new byte[] { 0x12, 0x00 }.Concat(rgbData).ToArray();
-            }
+            rgbData = new byte[] { 0x12, 0x00 }.Concat(rgbData).ToArray();
 
             int totalBytes = rgbData.Length;
-            int initialPacketSize = LightingCommanderCoreConfig.byteCount - initialHeaderSize;
+            int initialPacketSize = LightingCommanderCoreConfig.ByteCount - initialHeaderSize;
 
             WriteLighting(rgbData.Length, rgbData.Take(initialPacketSize).ToArray());
 
@@ -79,7 +140,7 @@ namespace LEDPlayground.Countrollers.Corsair
 
             while (totalBytes > 0)
             {
-                int bytesToSend = Math.Min(LightingCommanderCoreConfig.byteCount - headerSize, totalBytes);
+                int bytesToSend = Math.Min(LightingCommanderCoreConfig.ByteCount - headerSize, totalBytes);
                 StreamLighting(rgbData.Take(bytesToSend).ToArray());
 
                 totalBytes -= bytesToSend;
@@ -172,5 +233,64 @@ namespace LEDPlayground.Countrollers.Corsair
             };
             _stream.Write(packet.ToArray());
         }
+    }
+
+    public static class EliteLCDCooler
+    {
+        public static List<List<int>> Positioning { get; } = new List<List<int>>
+        {
+            new List<int>{6, 0},
+            new List<int>{5, 1},
+            new List<int>{7, 1},
+            new List<int>{4, 2},
+            new List<int>{8, 2},
+            new List<int>{3, 3},
+            new List<int>{9, 3},
+            new List<int>{2, 4},
+            new List<int>{10, 4},
+            new List<int>{1, 5},
+            new List<int>{11, 5},
+            new List<int>{0, 6},
+            new List<int>{12, 6},
+            new List<int>{1, 7},
+            new List<int>{11, 7},
+            new List<int>{2, 8},
+            new List<int>{10, 8},
+            new List<int>{4, 10},
+            new List<int>{8, 10},
+            new List<int>{3, 9},
+            new List<int>{9, 9},
+            new List<int>{5, 11},
+            new List<int>{7, 11},
+            new List<int>{6, 12}
+        };
+
+        public static List<int> Mapping { get; } = new List<int>
+        {
+            6,
+            5, 7,
+            4, 8,
+            3, 9,
+            2, 10,
+            1, 11,
+            0, 12,
+            23, 13,
+            22, 14,
+            21, 15,
+            20, 16,
+            19, 17,
+            18
+        };
+
+        public static List<string> LedNames { get; } = new List<string>
+        {
+            "Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10", "Led 11", "Led 12", "Led 13", "Led 14", "Led 15", "Led 16",
+            "Led 17", "Led 18", "Led 19", "Led 20", "Led 21", "Led 22", "Led 23", "Led 24"
+        };
+
+        public static string DisplayName { get; } = "Elite LCD Cooler";
+        public static int LedCount { get; } = 24;
+        public static int Width { get; } = 13;
+        public static int Height { get; } = 13;
     }
 }
